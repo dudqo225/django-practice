9f5c8c727772cf73953184fe7ca2e89f395d627b
dudqo225 <dudqo225@gmail.com>
2021-09-16 17:06:21 +0900

    97_auth add

---
+++ 97_auth/accounts/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import (
+    UserCreationForm, 
+    AuthenticationForm, 
+    UserChangeForm,
+    PasswordChangeForm
+)
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.http import require_POST
+from django.contrib.auth.models import User
+from .forms import CustomUserChangeForm
+
+# Create your views here.
+
+# 유저 목록 조회
+def index(request):
+    users = User.objects.all()
+    context = {
+        'users': users,
+    }
+    return render(request, 'accounts/index.html', context)
+
+# 회원가입
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+# 로그인
+def login(request):
+    # 이미 로그인한 경우
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'articles:index')
+            
+            # return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+# 로그아웃
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('accounts:login')
+
+
+# 프로필
+def profile(request, username):
+    user = User.objects.get(username=username)
+
+    context = {
+        'user': user,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+# 회원 삭제
+@require_POST
+def delete(request):
+    request.user.delete()
+    return redirect('accounts:login')
+
+
+# 회원 정보 수정
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:profile', request.user.username)
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+# 비밀번호 변경
+def password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = PasswordChangeForm(request.user)
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/password.html', context)
